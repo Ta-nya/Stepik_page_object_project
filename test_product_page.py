fce18cd9a17b9176b3a1efb71512fdf9387cc49b
Ta-nya <leeprae@gmail.com>
2020-07-15 01:40:45 +0300

    Add new tests Change "test_guest_cant_see_success_message_after_adding_product_to_basket", add new methods from ProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ from .pages.product_page import ProductPage
 import pytest
 import time
 
+
 @pytest.mark.parametrize('product_link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -18,6 +19,38 @@ def test_guest_can_add_product_to_basket(browser, product_link):
     product_page.open()
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
-    product_page.check_added_product()
+    product_page.check_added_product_price()
+    product_page.check_added_product_title()
     time.sleep(1)
 
+
+product_link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_disappear_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.go_to_login_page()
